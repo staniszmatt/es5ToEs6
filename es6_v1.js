fe5940cbce5d0dc5e4de85336813d2a9664c7470
staniszmatt <matthewpstaniszewski@gmail.com>
2019-01-28 15:06:15 -0800

    Got the image capture, need to decode it next

--- es6_v1.js
+++ es6_v1.js
@@ -1,8 +1,4 @@
-
-
-
-
-var animals = [
+const animals = [
 	{ type: 'giraffe', height: 8.2, weight: 315, name: 'Geofrie', top3Foods: ['leaves','roots','children']},
 	{ type: 'tiger', height: 2.1, weight: 120, name: 'Tony', top3Foods: ['meat','children','corn flakes']}
 ]
@@ -14,20 +10,24 @@ function animalListing( animalArray, listFoods){
 	if(listFoods===undefined){
 		listFoods = true;
 	}
-	for(var animalIndex = 0; animalIndex< animalArray.length; animalIndex++){
-		var thisAnimal = animalArray[animalIndex];
-		var name = thisAnimal.name;
-		var height = thisAnimal.height;
-		var weight = thisAnimal.weight;
-		var type = thisAnimal.type;
-		var foods = thisAnimal.top3Foods;
-		var food1 = foods[0];
-		var food2 = foods[1];
-		var food3 = foods[2];
-		var sentence = "Animal: "+name+" is a "+type+" and is "+height+" meters at "+(weight*2.2)+" lbs";
+	for(let animalIndex = 0; animalIndex< animalArray.length; animalIndex++){
+		let foodSentence;
+
+		let thisAnimal = animalArray[animalIndex];
+		let {name, height, type, weight} = animals;
+		// let name = thisAnimal.name;
+		// let height = thisAnimal.height;
+		// let weight = thisAnimal.weight;
+		// let type = thisAnimal.type;
+		const foods = thisAnimal.top3Foods;
+		let [food1, food2, food3] = foods; 
+		// let food1 = foods[0];
+		// let food2 = foods[1];
+		// let food3 = foods[2];
+		const sentence = "Animal: "+name+" is a "+type+" and is "+height+" meters at "+(weight*2.2)+" lbs";
 		console.log(sentence);
 		if(listFoods){
-			var foodSentence = "   it likes to eat "+food1+", "+food2+", and "+food3;
+			foodSentence = "   it likes to eat "+food1+", "+food2+", and "+food3;
 			console.log(foodSentence)
 		}
 	}
